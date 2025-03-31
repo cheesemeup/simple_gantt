792fdd7058abdf4fafa983daa3a9cef9231d4a93
cheeseplease <enzbenz@gmx.ch>
2025-03-31 14:39:31 +0200

    config file is generated and read properly

---
+++ run_simple_gantt.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+#
+# run_simple_gantt.sh
+# Author: cheesemeup
+#
+# runscript that contains the configuration for the simple_gantt script, and runs it
+
+# configuration
+cat << EOF > simple_gantt_config.cnf
+{
+    "work_packages":[[0,15],[15,27],[27,36]],
+    "figure_title":"Project Timeline",
+    "figure_filename":"timeline_gantt.png"
+}
+EOF
+
+# run simple_gantt
+python src/simple_gantt.py simple_gantt_config.cnf
+
